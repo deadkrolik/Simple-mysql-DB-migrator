40718c3779e72ccf47a74a107f19887ebd2d4fe9
Nevidimka <deadkrolik@gmail.com>
2012-04-04 17:45:59 +0600

    транслит в маленькие буквы, неправильная подмена имени класса

--- migrator/stagecreator.php
+++ migrator/stagecreator.php
@@ -44,8 +44,8 @@ class StageCreator {
 		$stage_template = file_get_contents(Migrator::get_base_dir().'/migrator/data/stage.tpl');
 		$stage_template = str_replace(array(
 			
-			'StageClassName', 
-			'StageComment'
+			'StageComment',
+			'StageClassName'
 		), array(
 			
 			str_replace(array("'","\n"), array("\'",''), $stage_name),//комментарий расположен в одиночных кавычках
@@ -71,7 +71,6 @@ class StageCreator {
 
 		//путь для сохранения файла с созданной миграцией
 		$stage_date = date('Y-m-d');
-		$stage_crc  = crc32($stage_name);
 		$stage_part = StageCreator::transliterate($stage_name);
 		$stage_file = Migrator::get_base_dir().'/db_changes/'.$stage_date.'['.$timestamp.']_'.$stage_part.'.php';
 		
@@ -97,13 +96,13 @@ class StageCreator {
 			'к' => 'k','л' => 'l','м' => 'm','н' => 'n','о' => 'o','п' => 'p',
 			'р' => 'r','с' => 's','т' => 't','у' => 'u','ф' => 'f','х' => 'h',
 			'ц' => 'c','ч' => 'ch','ш' => 'sh','щ' => 'sch','ь' => '','ы' => 'y',
-			'ъ' => '','э' => 'e','ю' => 'yu','я' => 'ya','А' => 'A','Б' => 'B',
-			'В' => 'V','Г' => 'G','Д' => 'D','Е' => 'E','Ё' => 'E','Ж' => 'Zh',
-			'З' => 'Z','И' => 'I','Й' => 'Y','К' => 'K','Л' => 'L','М' => 'M',
-			'Н' => 'N','О' => 'O','П' => 'P','Р' => 'R','С' => 'S','Т' => 'T',
-			'У' => 'U','Ф' => 'F','Х' => 'H','Ц' => 'C','Ч' => 'Ch','Ш' => 'Sh',
-			'Щ' => 'Sch','Ь' => '','Ы' => 'Y','Ъ' => '','Э' => 'E','Ю' => 'Yu',
-			'Я' => 'Ya',' ' => '_', '1' => '1','2' => '2','3' => '3','4' => '4',
+			'ъ' => '','э' => 'e','ю' => 'yu','я' => 'ya','А' => 'a','Б' => 'b',
+			'В' => 'v','Г' => 'g','Д' => 'd','Е' => 'e','Ё' => 'e','Ж' => 'zh',
+			'З' => 'z','И' => 'i','Й' => 'y','К' => 'k','Л' => 'l','М' => 'm',
+			'Н' => 'n','О' => 'o','П' => 'p','Р' => 'r','С' => 's','Т' => 't',
+			'У' => 'u','Ф' => 'f','Х' => 'h','Ц' => 'c','Ч' => 'ch','Ш' => 'sh',
+			'Щ' => 'sch','Ь' => '','Ы' => 'y','Ъ' => '','Э' => 'e','Ю' => 'yu',
+			'Я' => 'ya',' ' => '_', '1' => '1','2' => '2','3' => '3','4' => '4',
 			'5' => '5','6' => '6','7' => '7','8' => '8','9' => '9','0' => '0');
 		
 		$result = strtr($string, $converter);
